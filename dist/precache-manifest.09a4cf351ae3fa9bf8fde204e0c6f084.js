66c70ffbfc312245419a1da32fb34fddd6db19b6
“Jason <jason@techinvestmentgroup.com>
2020-08-06 03:33:06 -0400

    taco2

---
+++ dist/precache-manifest.09a4cf351ae3fa9bf8fde204e0c6f084.js
@@ -0,0 +1,250 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e62c96c51ed6f5a5d00a",
+    "url": "/css/about.fb9568df.css"
+  },
+  {
+    "revision": "d0239614595a8d949d50",
+    "url": "/css/chunk-vendors.6aaa0a9a.css"
+  },
+  {
+    "revision": "ee8bfa93debf09353b3029b7828e5ae8",
+    "url": "/img/apps-website-restaurant-marketing-ordering.ee8bfa93.jpg"
+  },
+  {
+    "revision": "89fa18c052d2668e348989a442bf0667",
+    "url": "/img/brioso-logo.89fa18c0.png"
+  },
+  {
+    "revision": "26670293ac37b9e3533f9dac2b686187",
+    "url": "/img/consultation.26670293.svg"
+  },
+  {
+    "revision": "2a7c119687c56ca4fa8017e39b971fb5",
+    "url": "/img/consultation1.2a7c1196.svg"
+  },
+  {
+    "revision": "c6e650e1147fe9cf70f2e9f2b2473823",
+    "url": "/img/consultation4.c6e650e1.svg"
+  },
+  {
+    "revision": "2e9d65087ad1bab1425208096d545114",
+    "url": "/img/consultation8.2e9d6508.svg"
+  },
+  {
+    "revision": "2961e9880210240c7e8a6983cbbb69c8",
+    "url": "/img/create-account.2961e988.jpg"
+  },
+  {
+    "revision": "ca2c419d39d998a2d9c5d0a1d4f444da",
+    "url": "/img/delivery-restaurant.ca2c419d.jpg"
+  },
+  {
+    "revision": "881050a3f06cbda1e3f55dbd86de5da8",
+    "url": "/img/email-marketing-restaurant-local.881050a3.jpg"
+  },
+  {
+    "revision": "0cbd89cf3abdd3fe4be346d8fff54bc3",
+    "url": "/img/facebook-restaurant-marketing.0cbd89cf.jpg"
+  },
+  {
+    "revision": "242bc982a2f1ef0e7da82267097ad5ce",
+    "url": "/img/fb-web-apps.242bc982.jpg"
+  },
+  {
+    "revision": "901971a3a2adde9f8d33479486cda0fa",
+    "url": "/img/info-graphic-icons.901971a3.png"
+  },
+  {
+    "revision": "3f5965d468a70cf59696f5ea608b27a7",
+    "url": "/img/local-citations.3f5965d4.jpg"
+  },
+  {
+    "revision": "a0dbfa49d1ade0e675d9a5be06cc3fcc",
+    "url": "/img/local-restaurant-marketing.a0dbfa49.jpg"
+  },
+  {
+    "revision": "bee87a5ebb6f831787dfd428c28d3c19",
+    "url": "/img/logo-bing.bee87a5e.png"
+  },
+  {
+    "revision": "40f7da7d38a6b1cc2c2bcb100efe6df4",
+    "url": "/img/logo-facebook.40f7da7d.png"
+  },
+  {
+    "revision": "d17aedde104132fe1912318a0d282064",
+    "url": "/img/logo-google.d17aedde.png"
+  },
+  {
+    "revision": "6eff0fcf8ceaa88df1aa5848c38e81c5",
+    "url": "/img/logo-instagram.6eff0fcf.png"
+  },
+  {
+    "revision": "27bcd4dac3cb5ca52fd811de1c303fc3",
+    "url": "/img/logo-pinterest.27bcd4da.png"
+  },
+  {
+    "revision": "04283b1c4b3cd82ed3795b402e7d7e92",
+    "url": "/img/loyalty-pos-apps.04283b1c.jpg"
+  },
+  {
+    "revision": "37afab387a53afad21959aa525a262ec",
+    "url": "/img/marketing-pos-cloud.37afab38.svg"
+  },
+  {
+    "revision": "2133df1c9072ed3e5202e6eb7e57ee68",
+    "url": "/img/media-connect.2133df1c.jpg"
+  },
+  {
+    "revision": "43f07c41d07f02f8489f719c65158001",
+    "url": "/img/menu-upates-restaurant.43f07c41.jpg"
+  },
+  {
+    "revision": "44447e90afda7152562f7e1a1553dc39",
+    "url": "/img/mobile-apps-restaurant.44447e90.jpg"
+  },
+  {
+    "revision": "e17fcc3acbbe95552bf4b40b3ab9c4fc",
+    "url": "/img/mobile-pos-apps.e17fcc3a.jpg"
+  },
+  {
+    "revision": "01c998ea487cf8c1f57efa14014a05b1",
+    "url": "/img/mobile-restaurant-delivery.01c998ea.jpg"
+  },
+  {
+    "revision": "d957cb6442b801cad2ed557578d24d9d",
+    "url": "/img/mobile-restaurant-web-design-logo.d957cb64.png"
+  },
+  {
+    "revision": "71983a0f96d1862c007bd95411738976",
+    "url": "/img/mock-training.71983a0f.svg"
+  },
+  {
+    "revision": "64d08b31cc9649fddb5d53e15f131805",
+    "url": "/img/online-ordering-restaurant-web-design.64d08b31.jpg"
+  },
+  {
+    "revision": "0577694ed40eb80d05dbc6e73ce9866a",
+    "url": "/img/online-ordering-terminal-restaurant.0577694e.jpg"
+  },
+  {
+    "revision": "e47e5607b202e27ca5d6d7c264753be1",
+    "url": "/img/order-complete.e47e5607.jpeg"
+  },
+  {
+    "revision": "b1c843e35d347de279e1acb925fc1fea",
+    "url": "/img/order-history-restaurants.b1c843e3.jpg"
+  },
+  {
+    "revision": "e5d230e64ee0e0ab89f45af76f6e87b5",
+    "url": "/img/payment-processing-restaurant.e5d230e6.jpg"
+  },
+  {
+    "revision": "2363477a78f0328161191afb7d2a53d6",
+    "url": "/img/pos-dashboard-business.2363477a.jpg"
+  },
+  {
+    "revision": "a3f842d727a3eab46446114fd1314bee",
+    "url": "/img/pricing-pos.a3f842d7.jpg"
+  },
+  {
+    "revision": "ef3c8c418f0833fbb920a61ba2c78598",
+    "url": "/img/reports-pos-best.ef3c8c41.jpg"
+  },
+  {
+    "revision": "03b165657436db7cedaf55a61277683c",
+    "url": "/img/restaurant-analytics.03b16565.jpg"
+  },
+  {
+    "revision": "e30640d0812b1a95b6727cf2f5521ee2",
+    "url": "/img/restaurant-app3.e30640d0.jpg"
+  },
+  {
+    "revision": "dd01bb22296e0b80e8fe9cb8bcdb1e76",
+    "url": "/img/restaurant-chef-order.dd01bb22.jpg"
+  },
+  {
+    "revision": "836c5404e6f017cbee3fa90e8471bd30",
+    "url": "/img/restaurant-dashboard.836c5404.jpg"
+  },
+  {
+    "revision": "481cc084e19fa47479f8e4bdcb13bc99",
+    "url": "/img/restaurant-image-mailer.481cc084.jpg"
+  },
+  {
+    "revision": "9e3c13b0889bc87748cb0a6faa7269f6",
+    "url": "/img/restaurant-image-social.9e3c13b0.jpg"
+  },
+  {
+    "revision": "7d44398497e40cb2615ee20e166ac5d5",
+    "url": "/img/restaurant-loyalty-program.7d443984.jpg"
+  },
+  {
+    "revision": "28e70b6fab3e09914eb43c890dd1d320",
+    "url": "/img/restaurant-menu-update-web.28e70b6f.jpg"
+  },
+  {
+    "revision": "b26f4432ecc0918e3ebb0529244fcf30",
+    "url": "/img/restaurant-mobile-apps-ri.b26f4432.jpg"
+  },
+  {
+    "revision": "e55679ec11b162422e0ccf8f1e078778",
+    "url": "/img/restaurant-online-ordering.e55679ec.jpg"
+  },
+  {
+    "revision": "0575405e35edd3420374c238a512640f",
+    "url": "/img/restaurant-send-promotional-offers.0575405e.jpg"
+  },
+  {
+    "revision": "34682324e7faf40c762bba55b77961de",
+    "url": "/img/restaurant-suite-marketing.34682324.jpg"
+  },
+  {
+    "revision": "2c852c8a0b32e8473648c1affdb343b8",
+    "url": "/img/restaurant-targeting.2c852c8a.jpg"
+  },
+  {
+    "revision": "5d0c2b200936865a62564294375686dc",
+    "url": "/img/retargeting.5d0c2b20.jpg"
+  },
+  {
+    "revision": "ad1cdfe58100afb5ee5f3b75c076a620",
+    "url": "/img/search-engine-marketing.ad1cdfe5.jpg"
+  },
+  {
+    "revision": "fd95aad44b43a3a5983c5f24d6a6d9e6",
+    "url": "/img/team-training-pos.fd95aad4.svg"
+  },
+  {
+    "revision": "1ab2e57a94fbff3a81b3685b031df1a2",
+    "url": "/img/yumapos-restaurant-terminal.1ab2e57a.jpg"
+  },
+  {
+    "revision": "57f78d71768f102f963ea26156a4c472",
+    "url": "/img/yumapos-restaurant2.57f78d71.jpg"
+  },
+  {
+    "revision": "91fdbc853f782b9bf8ee6e5072f06fe7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e62c96c51ed6f5a5d00a",
+    "url": "/js/about.4e9dfc8d.js"
+  },
+  {
+    "revision": "446981c71c4da6560d09",
+    "url": "/js/app.5ed0b2d1.js"
+  },
+  {
+    "revision": "d0239614595a8d949d50",
+    "url": "/js/chunk-vendors.3ac71053.js"
+  },
+  {
+    "revision": "70290cd5762029e42a23cd147eb9a34e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
